a71206d1bec41a72a5cf42c2265b32c0b2105a1b
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-04-06 20:17:23 +0530

    Complete update user profile page functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
@@ -7,9 +7,9 @@ export const verifyToken = (req, res, next) => {
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            return next(errorHandler(401, 'Unauthorized'))
+            return next(errorHandler(401, 'Unauthorized'));
         }
         req.user = user;
         next();
-    })
-}
+    });
+};
